3d9c15c8953f1b9257c39def7c48100842ade02c
Ronaldo Junior <ronaldomjunior05@gmail.com>
2025-08-03 23:35:52 -0300

    fix: execute mathematical expression correctly

--- src/env.ts
+++ src/env.ts
@@ -8,14 +8,16 @@ const envSchema = z.object({
   VITE_MAX_FILE_SIZE: z
     .string()
     .transform((val) => {
-      const size = Number.parseInt(val, 10)
+      // biome-ignore lint/security/noGlobalEval: <mathematic expression>
+      const size = eval(val)
+
       if (Number.isNaN(size)) {
         throw new Error("Invalid file size")
       }
       return size
     })
     .default(104_857_600), // 100 MB
-  VITE_MAX_FILE_COUNT: z.number().min(1).default(5),
+  VITE_MAX_FILE_COUNT: z.coerce.number().min(1).default(5),
   VITE_ALLOWED_EXTENSIONS: z.string().default(".zip"),
 })
 
