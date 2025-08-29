b6ffc65dbe3c5a393c0da6e061fb84de76c2bc76
Ronaldo Junior <ronaldomjunior05@gmail.com>
2025-08-29 16:58:34 -0300

    feat: implemente client global store

--- src/store/client-store.ts
+++ src/store/client-store.ts
@@ -1,13 +1,36 @@
 import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
 
 export type Client = {
   id: string
 }
 
 export interface ClientStore {
-  clients: Client[]
+  client: Client | undefined
   actions: {
     addClient: (client: Client) => void
-    removeClient: (id: string) => void
+    clearClient: () => void
   }
 }
+
+export const useClientStore = create<ClientStore>()(
+  persist(
+    (set) => ({
+      client: undefined,
+      actions: {
+        addClient: (client: Client) =>
+          set(() => ({
+            client,
+          })),
+        clearClient: () => set(() => ({ client: undefined })),
+      },
+    }),
+    {
+      name: "@email-send-1.0:client-store",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        client: state.client,
+      }),
+    }
+  )
+)
